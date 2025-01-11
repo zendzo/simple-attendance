65647e4a79212a7a7f01b38b6c3d0aeaf29ab0b4
M.Z Mustofa <zendzzo@gmail.com>
2025-01-11 09:00:35 +0700

    --wip-- [skip ci]

--- app/Filament/App/Pages/Leave.php
+++ app/Filament/App/Pages/Leave.php
@@ -2,9 +2,9 @@
 
 namespace App\Filament\App\Pages;
 
-use Filament\Pages\Page;
 use Filament\Actions;
 use Filament\Forms;
+use Filament\Pages\Page;
 
 class Leave extends Page
 {
@@ -12,45 +12,48 @@ class Leave extends Page
 
     protected static string $view = 'filament.app.pages.leave';
 
-     protected static ?string $navigationLabel = 'Izin | Cuti';
+    protected static ?string $navigationLabel = 'Izin | Cuti';
 
-     protected static ?string $title = 'Cuti Karayawan';
+    protected static ?string $title = 'Cuti Karayawan';
 
-     protected static ?int $navigationSort = 2;
-     
-    /**
-     * @return int|array
-     */
-     public function getHeaderWidgetsColumns(): int|array
+    protected static ?int $navigationSort = 2;
+
+    public function getHeaderWidgetsColumns(): int|array
     {
         return 1;
     }
 
-  protected function getActions(): array
-  {
-      return [
-        Actions\Action::make('create')
-          ->label('Ajuakan Izin | Cuti')
-          ->icon('heroicon-o-calendar-date-range')
-          ->modalHeading('Buat izin baru')
-          ->button()
-          ->form([
-            Forms\Components\TextInput::make('reason')
-              ->label('Alasan Cuti')
-              ->required(),
-            Forms\Components\DatePicker::make('start_date')
-              ->label('Tanggal Mulai')
-              ->required(),
-            Forms\Components\DatePicker::make('end_date')
-              ->label('Tanggal Selesai')
-              ->required(),
-          ])
-          ->action(function (array $data) {
-            // Handle the form submission
-            // For example, you can create a new leave record in the database
-            auth()->user()->leaves()->create($data);
-          }),
-      ];
+    protected function getActions(): array
+    {
+        return [
+            Actions\Action::make('create')
+                ->label('Ajuakan Izin | Cuti')
+                ->icon('heroicon-o-calendar-date-range')
+                ->modalHeading('Buat izin baru')
+                ->button()
+                ->form([
+                    Forms\Components\Select::make('reason')
+                        ->label('Alasan Cuti')
+                        ->options([
+                            'sakit' => 'Sakit',
+                            'izin' => 'Izin',
+                            'cuti' => 'Cuti',
+                            'lainnya' => 'Lainnya',
+                        ])
+                        ->required(),
+                    Forms\Components\DatePicker::make('start_date')
+                        ->label('Tanggal Mulai')
+                        ->required(),
+                    Forms\Components\DatePicker::make('end_date')
+                        ->label('Tanggal Selesai')
+                        ->required(),
+                ])
+                ->action(function (array $data) {
+                    // Handle the form submission
+                    // For example, you can create a new leave record in the database
+                    auth()->user()->leaves()->create($data);
+                }),
+        ];
     }
 
     public function getHeaderWidgets(): array
